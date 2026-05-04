51dfd6161a95089b29350a91b131581085a9ac1a
Damali Baker <damalib11@gmail.com>
2026-05-04 13:19:39 -0400

    Initialize Project

---
+++ src/main/java/ca/sheridancollege/bakerdam/authsystemapi/AuthSystemApiApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.bakerdam.authsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSystemApiApplication.class, args);
+    }
+
+}
