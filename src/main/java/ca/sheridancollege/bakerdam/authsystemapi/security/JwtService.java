9d3a9b3f857cf743e460322d9173eaab08aa5d8d
Damali Baker <damalib11@gmail.com>
2026-05-11 15:07:16 -0400

    Update jwt token generation to modern API

--- src/main/java/ca/sheridancollege/bakerdam/authsystemapi/security/JwtService.java
+++ src/main/java/ca/sheridancollege/bakerdam/authsystemapi/security/JwtService.java
@@ -1,31 +1,33 @@
 package ca.sheridancollege.bakerdam.authsystemapi.security;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtService {
-    private String secretKey;
-    private long expiration;
+    private final SecretKey secretKey;
+    private final long expiration;
 
     public JwtService(
             @Value("${jwt.secret}") String secretKey,
             @Value("${jwt.expiration}") long expiration
     ) {
-        this.secretKey = secretKey;
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
         this.expiration = expiration;
     }
 
     public String generateToken(String email) {
         return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
                 .compact();
     }
 }
